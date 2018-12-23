5f1aa99e9dd852757ae8cea917fedd4c617d4030
Henson Tauro <hensontauro@yahoo.com>
2018-12-23 18:14:44 +0530

    Learning curve

--- machine-learning-ex5/ex5/validationCurve.m
+++ machine-learning-ex5/ex5/validationCurve.m
@@ -39,6 +39,17 @@ error_val = zeros(length(lambda_vec), 1);
 %
 %
 
+for i = 1:length(lambda_vec)
+  
+  lambda = lambda_vec(i);
+  
+  theta_temp = trainLinearReg(X, y, lambda);
+  
+  error_train(i) = linearRegCostFunction(X, y, theta_temp, 0);
+  
+  error_val(i) = linearRegCostFunction(Xval, yval, theta_temp, 0);
+  
+end
 
 
 
